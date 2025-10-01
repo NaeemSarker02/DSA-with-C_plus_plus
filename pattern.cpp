f6fc9b4df3b937f6334e6f13e548a76ea1ff9456
NAEEM SARKER <naeemsarker202@gmail.com>
2025-10-01 13:45:42 +0600

    patterns

--- pattern.cpp
+++ pattern.cpp
@@ -7,17 +7,44 @@ void pattern(int n)
     {
         for (int j = 1; j <= n; j++)
         {
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
 }
+
+void pattern1(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
 
-    pattern(n);
+    // pattern(n);
+    // pattern1(n);
+    pattern2(n);
 
     // for (int i = 1; i <= n; i++)
     // {

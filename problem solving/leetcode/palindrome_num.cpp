83f6e77c496537711782c27a65016bb480a16f59
NAEEM SARKER <naeemsarker202@gmail.com>
2026-03-03 22:12:09 +0600

    f

--- problem solving/leetcode/palindrome_num.cpp
+++ problem solving/leetcode/palindrome_num.cpp
@@ -5,7 +5,7 @@ int main() {
     int x;
     cin >> x;
 
-    // Negative numbers are not palindrome
+
     if (x < 0) {
         cout << "false";
         return 0;
@@ -14,7 +14,7 @@ int main() {
     int original = x;
     int rev = 0;
 
-    // Reverse the number
+
     while (x > 0) {
         int digit = x % 10;      // take last digit
         rev = rev * 10 + digit;  // build reversed number
@@ -27,5 +27,6 @@ int main() {
     else
         cout << "false";
 
+        
     return 0;
 }

2027029ba42be4a812990362f0534c4c4d885d5d
NAEEM SARKER <naeemsarker202@gmail.com>
2026-03-07 23:17:26 +0600

    d

--- pattern/pattern.cpp
+++ pattern/pattern.cpp
@@ -75,12 +75,10 @@ void pattern5(int n)
 
 void pattern6(int n){ //**pyramid pattern
     for(int i = 0; i < n; i++){
-
-       
         for(int j = 0; j < n-i-1; j++){
             cout<<" ";
         }
-     
+    
         for(int j = 0; j < 2*i+1; j++){
             cout<<"*";
         }
